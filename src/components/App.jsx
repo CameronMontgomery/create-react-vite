44cd119daa7025b9b00c167541c4c93e02a6eac6
Cameron Montgomery <jc.montgomery0102@gmail.com>
2023-01-02 09:40:10 -0700

    Refactor css import; Add assets for README.md and Initial render of template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,5 @@
-import { createRoot } from "react-dom/client";
-
 const App = () => {
-  return (
-    <div className="app">
-      React + Vite
-    </div>
-  );
+  return <div className="app">React + Vite</div>;
 };
 
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<App />);
+export default App;
